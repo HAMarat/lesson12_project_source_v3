d02e151bd0757ab72a43a3cd731055a1340d4086
Huawei_station <smile56680@gmail.com>
2022-11-08 14:17:27 +0300

    proces... 2

--- functions.py
+++ functions.py
@@ -1,11 +1,12 @@
 import json
 
+JSON_POST_PATH = 'posts.json'
 
 def load_json_posts() -> list[dict]:
     """
     Загружаем данные из json файла
     """
-    with open('posts.json', 'r', encoding='utf-8') as file:
+    with open(JSON_POST_PATH, 'r', encoding='utf-8') as file:
         posts = json.load(file)
         return posts
 
@@ -17,3 +18,10 @@ def find_posts(text_search):
             find_posts_dict.append(post)
     return find_posts_dict
 
+
+def append_json_data(pic, content):
+    json_data = load_json_posts()
+    with open(JSON_POST_PATH, 'w', encoding='utf-8') as file:
+        post_dict = {'pic': pic, 'content': content}
+        json_data.append(post_dict)
+        json.dump(json_data, file, ensure_ascii=False, indent=4)
--- loader/loader.py
+++ loader/loader.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request
+from functions import append_json_data
 loader = Blueprint('loader', __name__, template_folder='templates', static_folder='static')
 
 
 @loader.route('/post')
 def load_post():
     return render_template('post_form.html')
+
+
+@loader.route('/post_uploaded', methods=['POST'])
+def post_uploaded():
+    post_picture = request.files.get('picture')
+    file_name = post_picture.filename
+    path_load = f'./uploads/images/{file_name}'
+    post_picture.save(path_load)
+
+    post_text = request.form.get('content')
+
+    append_json_data(path_load, post_text)
+
+    return render_template('post_uploaded.html', post_text=post_text, path_load=path_load)
