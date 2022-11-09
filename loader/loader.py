a2d5f7b1ea08c9991961531ccb701afc2cd06466
HAMarat <smile56680@gmail.com>
2022-11-09 23:08:48 +0300

    2

--- loader/loader.py
+++ loader/loader.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Blueprint, render_template, request
 from functions import append_json_data
 loader = Blueprint('loader', __name__, template_folder='templates', static_folder='static')
@@ -20,6 +22,10 @@ def post_uploaded():
     """
     # получаем объект из формы
     post_picture = request.files.get('picture')
+
+    if not post_picture:
+        logging.error("Файл не загружен")
+        return "Файл не загружен"
 
     # сохраняем название объекта
     file_name = post_picture.filename
